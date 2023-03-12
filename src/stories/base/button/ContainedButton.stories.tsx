d147b2d82205a7ae15a14ca0d7b32ee619d9399f
Kenzo-Wada <wada-kenzo@ed.tmu.ac.jp>
2023-03-12 14:37:06 +0900

    📚 create stories of buttons

---
+++ src/stories/base/button/ContainedButton.stories.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { ContainedButton } from '../../../components/base/button';
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Base/Button/ContainedButton',
+  component: ContainedButton,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+} as ComponentMeta<typeof ContainedButton>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof ContainedButton> = (args) => <ContainedButton {...args} />;
+
+export const Primary = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Primary.args = {
+  children: "Contained Button",
+};
---
+++ src/stories/base/button/OutlinedButton.stories.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { OutlinedButton } from '../../../components/base/button';
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Base/Button/OutlinedButton',
+  component: OutlinedButton,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+} as ComponentMeta<typeof OutlinedButton>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof OutlinedButton> = (args) => <OutlinedButton {...args} />;
+
+export const Primary = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Primary.args = {
+  children: "Outlined Button",
+};
