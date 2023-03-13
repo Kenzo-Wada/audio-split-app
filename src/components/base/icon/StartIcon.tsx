f2a37611d849504b3d82d1fc8f47c418e5ae465f
Kenzo-Wada <wada-kenzo@ed.tmu.ac.jp>
2023-03-13 17:00:48 +0900

    🍱 create start icon component

---
+++ src/components/base/icon/StartIcon.tsx
@@ -0,0 +1,13 @@
+import PlayArrowRoundedIcon from '@mui/icons-material/PlayArrowRounded';
+import { memo } from 'react';
+
+interface Props {
+}
+
+const StartIcon = memo((props: Props) => {
+  return <PlayArrowRoundedIcon />;
+})
+
+StartIcon.displayName = 'StartIcon';
+
+export default StartIcon;
