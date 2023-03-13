9a93cd85d5644ffe3782569da47217976801e72b
Kenzo-Wada <wada-kenzo@ed.tmu.ac.jp>
2023-03-13 16:25:16 +0900

    🍱 create default slider

---
+++ src/components/base/slider/DefaultSlider.tsx
@@ -0,0 +1,17 @@
+import Slider from '@mui/material/Slider';
+import styled from '@mui/system/styled';
+import { memo } from 'react';
+
+interface Props {
+}
+
+const StyledSlider = styled(Slider)({
+});
+
+const DefaultSlider = memo((props: Props) => {
+  return <StyledSlider />;
+});
+
+DefaultSlider.displayName = 'DefaultSlider';
+
+export default DefaultSlider;
