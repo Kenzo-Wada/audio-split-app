f8ab5ef0129c67f6e94c20a6b2735b7b11c5656f
Kenzo-Wada <wada-kenzo@ed.tmu.ac.jp>
2023-03-15 19:04:39 +0900

    :bento: create Paper comoponent

---
+++ src/components/base/paper/Paper.tsx
@@ -0,0 +1,13 @@
+import { Paper as MUIPaper } from '@mui/material';
+import type { PaperProps as MUIPaperProps } from '@mui/material/Paper';
+import { memo } from 'react';
+
+type PaperProps = MUIPaperProps;
+
+const Paper = memo((props: PaperProps) => {
+  return <MUIPaper {...props}/>;
+})
+
+Paper.displayName = 'Paper';
+
+export default Paper;
